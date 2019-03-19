b50a10491f5b7ad2c3c830a6024ee19dc4a0c549
skikirkwood <jim@jobflo.com>
2019-03-19 13:13:27 -0700

    Putting it together

--- bin/install-twd.sh
+++ bin/install-twd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script will test if you have given a leap year or not.
+# Initial attempt to automate creating a new demo environment space
 
 echo "Welcome to the Contentful Learning Demo installer!"
 echo "To get started we need to ask you a few things, then we'll build a fresh demo for you on your laptop."
@@ -7,12 +7,13 @@ echo "What is your Contentful Space ID?"
 read spaceid
 echo "What is your space's Content Delivery API token?"
 read cdatoken
-echo "We will use your Master environment for now"
+echo "We're going to create a new environment named demo."
 echo "Let's start!"
+contentful space environment create --environment-id 'demo' --name 'Demo' --space-id '$spaceid'
 # add locales here
 echo pwd
 echo "About to import content from contentful_exports/SEDemo.json"
-contentful space import --content-file contentful_exports/SEDemo.json --space-id $spaceid
+contentful space import --content-file contentful_exports/SEDemo.json --space-id '$spaceid' --environment-id 'demo'
 # run SED to edit config.js here
 echo "About to run npm install"
 npm install
