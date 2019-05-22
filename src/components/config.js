75c036bae4b3f65b09d84bd57a0441ff7cc43e1a
skikirkwood <jim@jobflo.com>
2019-05-22 16:27:13 -0700

    checking in modified config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   // /*preview host url*/ 
   // host: "preview.contentful.com",
-  space_id: '',
+  space_id: '7fv15sv8cwmq',
   /*master DELIVERY*/
-  delivery_token: '',
+  delivery_token: 'JudcVH9O9oc42O2j953Wu2C0isnPLfvI4DD8GSsVJT0',
   ///*master PREVIEW*/
   preview_token: '',
   environment: 'demo',
