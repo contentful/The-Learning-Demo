a781fb0f1658e78431007d3aadc3142a3d4eb0ab
skikirkwood <jim@jobflo.com>
2019-06-15 23:51:15 +0200

    Fixed up so SED works

--- src/components/config.js
+++ src/components/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   // /*preview host url*/ 
   // host: "preview.contentful.com",
-  space_id: '7fv15sv8cwmq',
+  space_id: '',
   /*master DELIVERY*/
-  delivery_token: 'JudcVH9O9oc42O2j953Wu2C0isnPLfvI4DD8GSsVJT0',
+  delivery_token: '',
   ///*master PREVIEW*/
   preview_token: '',
   environment: 'demo',
