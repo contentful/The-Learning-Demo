f48b45bf3ca55a666bbb361e0be5dc8b271a8eb1
skikirkwood <jim@jobflo.com>
2019-07-16 09:26:54 -0700

    Removing space ids and token ids

--- src/components/config.contentful-demo-eu.js
+++ src/components/config.contentful-demo-eu.js
@@ -1,5 +1,5 @@
 module.exports = {
-  space_id: '6wguvib64ij5',
-  delivery_token: '212d7e329af8d826ad6c649f43a849cfc6c0c27fd957e83655ea52859a58ca60',
-  preview_token: '2fc380eb75b35dd280ef7c9be9aaab2e9b1114aa0ada852a519cd3a0ca867256'
+  space_id: '',
+  delivery_token: '',
+  preview_token: ''
 }
--- src/components/config.contentful-demo-us.js
+++ src/components/config.contentful-demo-us.js
@@ -1,5 +1,5 @@
 module.exports = {
-  space_id: 'gnmwbhehd31i',
-  delivery_token: '0cc6fe519ed43619b536b9c743488c15e17fa7909197e63fe48f51732e900be8',
-  preview_token: 'a6f717d684dd0a8abaf319ace4b74a54b9476e3c37985c3d4d84698762a38aba'
+  space_id: '',
+  delivery_token: '',
+  preview_token: ''
 }
