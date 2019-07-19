93cf4ca6f9cca6580e7bc9f545afe765ca88ae91
skikirkwood <jim@jobflo.com>
2019-07-19 09:30:43 -0700

    cleaning up install files

--- bin/install.sh
+++ bin/install.sh
@@ -10,15 +10,15 @@ read cdatoken
 echo "Let's start!"
 echo "Deleting demo environment if it already exists"
 contentful space environment delete --environment-id 'demo' --space-id $spaceid
-echo "About to import content from contentful_exports/SEDemo.json"
-contentful space import --content-file contentful_exports/SEDemo.json --space-id $spaceid
+echo "About to import content from contentful_exports/tld.json"
+contentful space import --content-file contentful_exports/tld.json --space-id $spaceid
 sed -i -e "s/space_id: ''/space_id: '$spaceid'/" src/components/config.js
 sed -i -e "s/delivery_token: ''/delivery_token: '$cdatoken'/" src/components/config.js
 echo "We're going to create a new environment named demo."
 contentful space environment create --environment-id 'demo' --name 'Demo' --space-id $spaceid
 # add locales here
-echo "About to run npm install"
-npm install
-echo "To start your web server and demo type: npm run dev"
+echo "About to run npm install - you will need to enter your login password"
+sudo npm install
+echo -e "To start your web server and demo type: npm run dev"
 echo "To see the demo bring up a web browser and go to http://localhost:8080"
 
