44ad283679b5ca8c58467b7a683ad5e5df89e629
skikirkwood <jim@jobflo.com>
2021-02-10 16:22:20 -0800

    More cleanup

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-// import ProductsIndex from './components/product_index';
 import ProductShow from './components/product_show';
 
 export default (
   <Route path="/" component={App}>
-      <IndexRoute component={ProductShow}/>
+    <IndexRoute component={ProductShow}/>
     <Route path ="/:slug" component={ProductShow} />
   </Route>
 );
