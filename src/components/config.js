6eceae83e5169e2fd51e0b02cdee0474596cbec0
Chigozirim Kawa <kawa.chigozirim@gmail.com>
2021-06-24 11:46:42 +0200

    clean up

--- src/components/config.js
+++
@@ -1,7 +0,0 @@
-// All environment variables will be sourced
-// Do NOT commit this file to source control
-module.exports = {
-space_id: 's03wastxid7g',
-delivery_token: 'MQydt8bTk8lVwMHmcJqFs9fGqBheVvGa-JBsH1FZw8Q',
-environment: 'demo'
-}
